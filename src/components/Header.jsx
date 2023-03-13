3fa4d540b5619aa017ec3ec0fdd406870317a648
Luanderson Alves <luaoderson@gmail.com>
2023-03-13 16:33:37 -0300

    RE04: links on header are working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 export default class Header extends React.Component {
@@ -16,6 +17,32 @@ export default class Header extends React.Component {
     const { isLoading, nameUser } = this.state;
     return (
       <header data-testid="header-component">
+        <ul>
+          <li>
+            <Link
+              to="/search"
+              data-testid="link-to-search"
+            >
+              Procurar
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/favorites"
+              data-testid="link-to-favorites"
+            >
+              Favoritos
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/profile"
+              data-testid="link-to-profile"
+            >
+              Perfil
+            </Link>
+          </li>
+        </ul>
         {
           isLoading
             ? <span>Carregando...</span>
