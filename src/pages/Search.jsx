7abe11add6423754beaff0768b7a911c61ba637c
Luanderson Alves <luaoderson@gmail.com>
2023-03-13 16:41:45 -0300

    REQ05: form on search are done

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import Header from '../components/Header';
 
+const INPUT_LENGTH = 2;
+
 export default class Search extends React.Component {
+  state = {
+    inputValue: '',
+  };
+
+  fHandleChange = ({ target }) => {
+    const { value } = target;
+    this.setState(() => ({ inputValue: value }));
+  };
+
   render() {
+    const { inputValue } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <span>search page</span>
+          <form onSubmit={ (e) => { e.preventDefault(); } }>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              value={ inputValue }
+              onChange={ this.fHandleChange }
+            />
+            <button
+              data-testid="search-artist-button"
+              disabled={ inputValue.length < INPUT_LENGTH }
+            >
+              Pesquisar
+            </button>
+          </form>
         </div>
       </>
     );
