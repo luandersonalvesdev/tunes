83707c11508ba741e96e9c3efdee09f2ed345bbf
Luanderson Alves <luaoderson@gmail.com>
2023-03-15 21:24:09 -0300

    CSS: search page are done for now

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Loading from './Loading';
+import '../css/Search.css';
 
 const INPUT_LENGTH = 2;
 
@@ -14,52 +15,52 @@ export default class Search extends React.Component {
     artist: '',
   };
 
-  fReturnContainerAlbums = (searchAlbums) => {
-    const { artist } = this.state;
-    return (
-      <>
-        <span>
-          Resultado de álbuns de:
-          {` ${artist}`}
-        </span>
-        <section>
-          {
-            searchAlbums.map((album) => (
-              <Link
-                data-testid={ `link-to-album-${album.collectionId}` }
-                to={ `/album/${album.collectionId}` }
-                key={ album.collectionId }
-              >
-                <p>{album.artistName}</p>
-                <p>{album.collectionName}</p>
-                <p>{album.trackCount}</p>
-              </Link>
-            ))
-          }
-        </section>
-      </>
-    );
-  };
+  // fReturnContainerAlbums = (searchAlbums) => {
+  //   const { artist } = this.state;
+  //   return (
+  //     <>
+  //       <span>
+  //         Resultado de álbuns de:
+  //         {` ${artist}`}
+  //       </span>
+  //       <section>
+  //         {
+  //           searchAlbums.map((album) => (
+  //             <Link
+  //               data-testid={ `link-to-album-${album.collectionId}` }
+  //               to={ `/album/${album.collectionId}` }
+  //               key={ album.collectionId }
+  //             >
+  //               <p>{album.artistName}</p>
+  //               <p>{album.collectionName}</p>
+  //               <p>{album.trackCount}</p>
+  //             </Link>
+  //           ))
+  //         }
+  //       </section>
+  //     </>
+  //   );
+  // };
 
-  fReturnForm = () => {
-    const { inputValue } = this.state;
-    return (
-      <form onSubmit={ (e) => { e.preventDefault(); } }>
-        <input
-          data-testid="search-artist-input"
-          type="text"
-          value={ inputValue }
-          onChange={ this.fHandleChange }
-        />
-        <button
-          data-testid="search-artist-button"
-          disabled={ inputValue.length < INPUT_LENGTH }
-          onClick={ () => { this.fSearch(inputValue); } }
-        >
-          Pesquisar
-        </button>
-      </form>);
-  };
+  // fReturnForm = () => {
+  //   const { inputValue } = this.state;
+  //   return (
+  //     <form onSubmit={ (e) => { e.preventDefault(); } }>
+  //       <input
+  //         data-testid="search-artist-input"
+  //         type="text"
+  //         value={ inputValue }
+  //         onChange={ this.fHandleChange }
+  //       />
+  //       <button
+  //         data-testid="search-artist-button"
+  //         disabled={ inputValue.length < INPUT_LENGTH }
+  //         onClick={ () => { this.fSearch(inputValue); } }
+  //       >
+  //         Pesquisar
+  //       </button>
+  //     </form>);
+  // };
 
   fHandleChange = ({ target }) => {
     const { value } = target;
@@ -78,21 +79,60 @@ export default class Search extends React.Component {
   };
 
   render() {
-    const { isLoading, searchAlbums, search } = this.state;
+    const { isLoading, searchAlbums, search, inputValue, artist } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search">
+        <div data-testid="page-search" id="div-main-search">
           {
             isLoading
               ? <Loading />
-              : this.fReturnForm()
+              : (
+                <form onSubmit={ (e) => { e.preventDefault(); } }>
+                  <input
+                    data-testid="search-artist-input"
+                    type="text"
+                    value={ inputValue }
+                    onChange={ this.fHandleChange }
+                    placeholder="Digite o nome do artista."
+                  />
+                  <button
+                    data-testid="search-artist-button"
+                    disabled={ inputValue.length < INPUT_LENGTH }
+                    onClick={ () => { this.fSearch(inputValue); } }
+                  >
+                    Pesquisar
+                  </button>
+                </form>
+              )
           }
           {
             search
               && (!searchAlbums.length
-                ? <span>Nenhum álbum foi encontrado</span>
-                : this.fReturnContainerAlbums(searchAlbums)
+                ? <span id="span-notFound">Nenhum álbum foi encontrado</span>
+                : (
+                  <>
+                    <span id="span-found">
+                      Resultado de álbuns de
+                      <span id="span-artist">{artist}</span>
+                    </span>
+                    <section id="section-allAlbums">
+                      {
+                        searchAlbums.map((album) => (
+                          <Link
+                            data-testid={ `link-to-album-${album.collectionId}` }
+                            to={ `/album/${album.collectionId}` }
+                            key={ album.collectionId }
+                          >
+                            <p>{album.artistName}</p>
+                            <p>{album.collectionName}</p>
+                            <p>{album.trackCount}</p>
+                          </Link>
+                        ))
+                      }
+                    </section>
+                  </>
+                )
               )
           }
 
