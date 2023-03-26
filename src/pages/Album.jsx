ed6a013827d52639bc3cf52e6fdb986602053b7d
Luanderson Alves <luaoderson@gmail.com>
2023-03-26 17:26:47 -0300

    CSS: page album are done

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import getMusics from '../services/musicsAPI';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
+import '../css/Album.css';
 
 export default class Album extends React.Component {
   state = {
@@ -22,20 +23,27 @@ export default class Album extends React.Component {
       artistName: data[0].artistName,
       allMusics: data,
       collectionName: data[0].collectionName,
+      artworkUrl100: data[0].artworkUrl100,
     }));
   };
 
   render() {
-    const { artistName, allMusics, collectionName } = this.state;
+    const { artistName, allMusics, collectionName, artworkUrl100 } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-album">
-          <h2 data-testid="artist-name">{artistName}</h2>
-          <h3 data-testid="album-name">{collectionName}</h3>
-          <ul>
-            <MusicCard allMusics={ allMusics } />
-          </ul>
+        <div className="container-album" data-testid="page-album">
+          <div className="container-album-cover">
+            <img src={ artworkUrl100 } alt="" />
+          </div>
+          <div className="container-songs">
+            <img id="main-img" src={ artworkUrl100 } alt="" />
+            <h2 data-testid="artist-name">{artistName}</h2>
+            <h3 data-testid="album-name">{collectionName}</h3>
+            <ul>
+              <MusicCard allMusics={ allMusics } />
+            </ul>
+          </div>
         </div>
       </>
     );
